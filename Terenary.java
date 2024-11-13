fe780a2e112adc3dccc0000205c1b59ee57c026b
vivekchadhuvula01 <vivekchadhuvula@gmail.com>
2024-11-13 18:05:41 +0530

    terenary operators are done

---
+++ Terenary.java
@@ -0,0 +1,11 @@
+class Terenary{
+    public static void main(String[] args){
+        int x,y;
+        x=5;
+        y=6;
+
+        int rs = x<y ? 10 :20;  
+        // ? indicates if statement : indicates else statement;
+        System.out.println(rs);
+    }
+}
